775bf1b4ac51a9750e7ad96a836c42d5930354dd
Paige Finkelstein <paige@turn.io>
2023-01-05 13:32:50 -0500

    add setup for running method

--- pascals_triangle.py
+++ pascals_triangle.py
@@ -1,3 +1,22 @@
 class Solution:
     def generate(self, numRows):
+        print("pass")
         pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+   solution = Solution()
+   solution.generate(1)
