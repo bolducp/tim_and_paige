9d094b349398b9b8a6294b03ea89ef83552ea339
timothy battafarano <tim@deepsprings.edu>
2023-01-05 13:43:15 -0600

    solution

--- pascals_triangle.py
+++ pascals_triangle.py
@@ -5,6 +5,21 @@ class Solution:
         if numRows == 2:
             return [[1], [1, 1]]
 
+        rows = [[1], [1, 1]]
+
+        for x in range(numRows - 2):
+            next_row = [1]
+            for index in range(1, x + 2):
+                previous_row = rows[-1]
+                next_value = previous_row[index - 1] + previous_row[index]
+                next_row.append(next_value)
+
+            next_row.append(1)
+            rows.append(next_row)
+
+
+        return rows
+
 
 
 
@@ -21,4 +36,4 @@ class Solution:
 
 if __name__ == "__main__":
    solution = Solution()
-   print(solution.generate(2))
+   print(solution.generate(5))
